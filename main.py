f5a9088cbe8d6c881dddb8c9e0cdcfe52c691e98
zxcv0807 <pg8221@naver.com>
2025-07-19 16:31:18 +0900

    cors 수정

--- main.py
+++ main.py
@@ -11,7 +11,9 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://webreviewer.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
