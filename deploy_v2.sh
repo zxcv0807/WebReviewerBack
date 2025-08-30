eeffdc3311f141423bad740024e3d80fe9c3c37b
zxcv0807 <pg8221@naver.com>
2025-08-30 16:14:07 +0900

    test

--- deploy_v2.sh
+++ deploy_v2.sh
@@ -13,10 +13,19 @@ git reset --hard HEAD
 echo "➡️ 2. 최신 코드를 가져옵니다 (git pull)..."
 git pull
 
-echo "➡️ 3. Docker 컨테이너를 다시 빌드하고 시작합니다..."
+# ✏️ git 작업 이후에 .env 파일을 생성하여 삭제되는 것을 방지합니다.
+echo "➡️ 3. GitHub Secret으로부터 .env 파일을 생성합니다..."
+if [ -z "$ENV_FILE_CONTENT" ]; then
+  echo "🚨 에러: ENV_FILE_CONTENT 환경 변수가 설정되지 않았습니다."
+  exit 1
+fi
+echo "$ENV_FILE_CONTENT" > .env
+echo ".env 파일 생성이 완료되었습니다."
+
+echo "➡️ 4. Docker 컨테이너를 다시 빌드하고 시작합니다..."
 sudo docker-compose up --build -d
 
-echo "➡️ 4. 사용하지 않는 Docker 이미지를 정리하여 디스크 공간을 확보합니다..."
+echo "➡️ 5. 사용하지 않는 Docker 이미지를 정리하여 디스크 공간을 확보합니다..."
 sudo docker image prune -f
 
 echo "✅ 배포가 성공적으로 완료되었습니다!"
